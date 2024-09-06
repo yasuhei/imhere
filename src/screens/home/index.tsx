3e88a7b68c465f300c09e7e135d81edbde5cbab2
yasuhei nakamura <67479901+yasuhei@users.noreply.github.com>
2024-09-06 15:38:12 -0300

    removendo funcao

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -5,20 +5,28 @@ import { useState } from 'react';
 
 export  function Home() {
 
-  const [ name, setName ] = useState('')
-  const participants = ['yasuhei', 'samara', 'pedro','ana', 'isa', 'felipe', 'lucas', 'andre', 'joao']
+  const [ participant, setParticipant ] = useState<string[]>([])
+  const [participantName, setParticipantName] = useState('')
+
+
     function handleParticipantAdd(event: NativeSyntheticEvent<NativeTouchEvent>) {
         event.preventDefault()
-        if(participants.includes("yasuhei")) {
+        if(participant.includes(participantName)) {
          return Alert.alert("Participante existe", "Já existe um participante")
         }
+        setParticipant(prevState => [...prevState, participantName])
+        setParticipantName('')
     }
 
+
+
     function handleParticipantRemove(name: string) {
+
       Alert.alert("Remover", `Remover o participante ${name}?`, [
         {
           text: 'Sim',
-          onPress: () => Alert.alert("Deletado")
+          onPress: () =>  setParticipant(prevState => prevState.filter(participant => participant !== name)),
+
         },
         {
           text: "Não",
@@ -29,10 +37,15 @@ export  function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}> Nome do evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2024</Text>
+      <Text style={styles.eventDate}>Sexta, 5 de Novembro de 2024</Text>
 
       <View style={styles.form}>
-      <TextInput style={styles.input} placeholder='Nome do participante' placeholderTextColor="#6b6b6b"  />
+      <TextInput style={styles.input}
+       placeholder='Nome do participante' 
+       placeholderTextColor="#6b6b6b" 
+       onChangeText={setParticipantName}
+       value={participantName} />
+       
       <TouchableOpacity style={styles.button}  onPress={handleParticipantAdd}>
         <Text style={styles.buttonText}>
             +
@@ -46,7 +59,7 @@ export  function Home() {
           Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
         </Text>
       )}
-        data={participants}
+        data={participant}
         keyExtractor={item => item}
         renderItem={({item}) => (
           <Participant name={item} onRemove={() => handleParticipantRemove(item)} key={item} />
